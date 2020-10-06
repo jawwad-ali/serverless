152889eeab92468a65897d59415819d80cf905c0
jawwad-ali <alijawwad001@gmail.com>
2020-10-06 19:16:44 +0500

    serverless on netlify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+
+    fetch(`/.netlify/functions/hello?name=from react to netlify function`)
+      .then(response => response.json())
+      .then(apidata => setData(apidata))
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Hello from Serverless</h4>
+      {data.message}
     </div>
   );
 }
