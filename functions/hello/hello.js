4c4b1e11360d3ee485bc50ecebd9d1e1d0380c6e
jawwad-ali <alijawwad001@gmail.com>
2020-10-12 15:59:03 +0500

    added random name library

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,15 +1,40 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-exports.handler = async (event, context) => {
+// exports.handler = async (event, context) => {
+//   try {
+//     const subject = event.queryStringParameters.name || 'World'
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: `Hello ${subject}` }),
+//       // // more keys you can return:
+//       // headers: { "headerName": "headerValue", ... },
+//       // isBase64Encoded: true,
+//     }
+//   } catch (err) {
+//     return { statusCode: 500, body: err.toString() }
+//   }
+// }
+
+
+
+
+// RANDOM NAME
+var random = require('random-name')
+
+exports.handler = async (e, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    let first_name = random.first()
+    let last_name = random.last()
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Dear ${first_name} ${last_name}` }),
+
     }
-  } catch (err) {
+  }
+  catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
 }
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 function App() {
 
   const [data, setData] = useState({})
-  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
 
-    fetch(`/.netlify/functions/hello?name=from react to netlify function`)
-    setLoading(true)
-      .then(response => response.json())
-      .then(apidata => setData(apidata))
-    setLoading(false)
+    // fetch(`/.netlify/functions/hello?name=from react to netlify function`)
+    // setLoading(true)
+    //   .then(response => response.json())
+    //   .then(apidata => setData(apidata))
+    // setLoading(false)
+
+    fetch(`/.netlify/functions/hello`)
+      .then(resposne => resposne.json())
+      .then(data => {
+        setData(data)
+
+      })
 
   }, [])
 
 
-  if (loading)
-    return <h6 style={{ color: "blue" }}>loading...</h6>
 
   return (
     <div className="App">
